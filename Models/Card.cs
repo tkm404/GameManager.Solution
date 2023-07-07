168647597aa37f710f49e72481273664783e0b75
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-07-07 08:54:16 -0700

    add constructor references for card model.

--- Models/Card.cs
+++ Models/Card.cs
@@ -11,4 +11,21 @@ public class Card : MonoBehaviour
   public int defense;
   public string cardType;
   public string cardDescription;
+
+
+  public Card()
+  {
+
+  }
+
+  public Card(int Id, string CardName, int Power, int Defense, string CardType, string CardDescription)
+  {
+    id = Id;
+    cardName = CardName;
+    power = Power;
+    defense = Defense;
+    cardType = CardType;
+    cardDescription = CardDescription;
+  }
+
 }
